bec89a02b971bd21c15a53d3d33a99d6aab390ce
Kirill Klepinin <lucifersupport@70.ru>
2023-03-09 11:52:35 +0700

    Finished part 1 of the task

---
+++ Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Person
+    {
+        private string name;
+        private bool sex;
+        private int age;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public char Sex
+        {
+            get => sex ? 'F' : 'M';
+            set { switch (value) { case 'M': sex = false; break; case 'F': sex = true; break; } }
+        }
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+        public Person(string name, char sex, int age)
+        {
+            Alerter.Green("New person created.");
+            this.name = name;
+            switch (sex) { case 'M': this.sex = false; break; case 'F': this.sex = true; break; }
+            this.age = age;
+        }
+        public Person()
+        {
+            Alerter.Green("New person created");
+            this.name = "Undefined";
+            this.sex = false;
+            this.age = 0;
+        }
+        public void Print() 
+        {
+            Alerter.Green("Printing info."); 
+            Console.WriteLine($"Class: Person; Name: {this.Name}; Sex: {this.Sex}; Age: {this.Age}.");
+        }
+        public void Input()
+        {
+            Alerter.Green("Editing data.");
+
+#pragma warning disable CS8601 // Возможно, назначение-ссылка, допускающее значение NULL.
+            Console.Write("Name: "); this.Name = Console.ReadLine();
+#pragma warning restore CS8601 // Возможно, назначение-ссылка, допускающее значение NULL.
+
+            Console.Write("Sex (M/F): "); this.Sex = Char.ToUpper(Console.ReadKey().KeyChar);
+
+            Console.Write("\nAge: "); this.Age = Convert.ToInt32(Console.ReadLine());
+        }
+        ~Person() => Alerter.Red("Person \"" + this.Name + "\" terminated.");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,51 @@
 ﻿namespace ConsoleTest
 {
+    static class Alerter
+    {
+        public static void Red(string prompt)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Routine(prompt);
+        }
+        public static void Yellow(string prompt) {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Routine(prompt);
+        }
+        public static void Blue(string prompt)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Routine(prompt);
+        }
+        public static void Green(string prompt)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Routine(prompt);
+        }
+        static void Routine(string prompt)
+        {
+            Console.WriteLine(prompt);
+            Console.ResetColor();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Alerter.Blue("--- PART 1 ---");
+
+            Person Bob = new("Bob", 'M', 19);
+            Bob.Print();
+
+            Person Customer = new();
+            Customer.Print();
+            Customer.Input();
+            Customer.Print();
+            
+            Console.ReadKey();
         }
     }
 }
